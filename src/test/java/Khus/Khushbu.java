204c098340cac46106dce542ed9761f31f70c6d7
khushbu <skhushbu761@gmail.com>
2023-08-29 15:15:48 +0530

    13th commit

--- src/test/java/Khus/Khushbu.java
+++ src/test/java/Khus/Khushbu.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 
 public class Khushbu
 {
-	@Test(priority=2, enabled= true)
+	@Test(priority=1, enabled= true)
 
 	public void GooglePixeldetails() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver",
@@ -21,13 +21,13 @@ public class Khushbu
         
         // Create a new ChromeDriver instance with the specified ChromeOptions
         WebDriver driver = new ChromeDriver(chromeOptions);
-		driver.get("http://13.126.143.65:3000/");
+		driver.get("http://43.205.230.7:3000/");
 
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//div[@class='container']//div[1]//div[1]//div[1]//a[1]")).click();
 	}
 
-	@Test(priority=1, enabled= true)
+	@Test(priority=2, enabled= true)
 
 	public void OpenCart() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver",
@@ -40,32 +40,34 @@ public class Khushbu
         // Create a new ChromeDriver instance with the specified ChromeOptions
         WebDriver driver = new ChromeDriver(chromeOptions);
 
-		driver.get("http://13.126.143.65:3000/");
+		driver.get("http://43.205.230.7:3000/");
 
 		Thread.sleep(3000);
-		driver.findElement(By.xpath("//i[normalize-space()='my cart']")).click();
-		Assert.fail();
+		//driver.findElement(By.xpath("//i[normalize-space()='my cart']")).click();
 
 	}
 
-	/*@Test(priority=3, enabled=true)
+	@Test(priority=3, enabled=true)
 
 	public void InCart() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\hp\\eclipse-workspace\\local_host\\src\\main\\resources\\chromedriver.exe");
+				"//home/ec2-user/chromedriver");
+		ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless"); // Run in headless mode
+        chromeOptions.addArguments("--disable-gpu"); // Disable GPU acceleration
+        
+        // Create a new ChromeDriver instance with the specified ChromeOptions
+        WebDriver driver = new ChromeDriver(chromeOptions);
 
-		// create driver
-		WebDriver driver = new ChromeDriver();
-
-		driver.get("http://localhost:3001");
+		driver.get("http://43.205.230.7:3000/");
 
 		Thread.sleep(3000);
 
 		driver.findElement(By.xpath("//i[normalize-space()='my cart']")).click();
 		String Actualresult = driver.getCurrentUrl();
 		System.out.println("Actual result : "+ Actualresult);
-           Assert.assertEquals(Actualresult,"http://localhost:3001/cart");
+           Assert.assertEquals(Actualresult,"http://43.205.230.7:3000/cart");
 
-	}*/
+	}
 
 }
