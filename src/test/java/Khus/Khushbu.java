1e3fa0fc6531ce2edb92c278bb9c3308bc2bf80e
Khushbu761 <140041758+Khushbu761@users.noreply.github.com>
2023-09-05 15:59:59 +0530

    Update Khushbu.java

--- src/test/java/Khus/Khushbu.java
+++ src/test/java/Khus/Khushbu.java
@@ -44,7 +44,7 @@ public class Khushbu
 
 		Thread.sleep(3000);
 		//driver.findElement(By.xpath("//i[normalize-space()='my cart']")).click();
-		System.out.println("Hello");
+		System.out.println("Hello   ");
 	}
 
 	@Test(priority=3, enabled=true)
